6f29a2a285940f61629858547880604bab53cdda
Michael <Jahnsmic001@gmail.com>
2018-10-29 12:21:15 -0700

    added shuffle and draw mechanics, roadblocked by game css/html

---
+++ app/app.js
@@ -0,0 +1,36 @@
+//Shuffle
+function shuffle(deck){
+  Card.cardDeck = deck.splice(0);
+  console.table(Card.cardDeck)
+  for (var i = Card.cardDeck.length - 1; i > 0; i--) {
+    var randomPosition = Math.floor(Math.random() * (i + 1));
+    var temp = Card.cardDeck[i];
+    Card.cardDeck[i] = Card.cardDeck[randomPosition];
+    Card.cardDeck[randomPosition] = temp;
+  }
+  console.table(Card.cardDeck)
+}
+
+function drawPhase(){
+//render first three cards in Card.cardDeck to page
+//   for(var i = 0; i <= 2; i++){
+//     var idReference = Card.cardDeck[i].id;
+//     for(var j = 0; j < Card.allCards.length; j++){
+//       if(idReference === Card.allCard[j].id){
+//           //render image
+//           //render name
+//           //render description
+//       }
+//     }
+//   }
+  Card.cardDeck.splice(0, 3);
+  console.table(Card.cardDeck)
+}
+
+function handleStart(){
+  console.log('event started')
+  shuffle(Card.allCards);
+}
+
+var startElement = document.getElementById('start');
+startElement.addEventListener('click', handleStart);
--- app/constructor.js
+++ app/constructor.js
@@ -1,67 +1,68 @@
-var allCards =[];
-function cardCreate(name,storyline, description){
+
+function Card(id, name,storyline, description){
+  this.id = id;  
   this.name = name;
   this.storyline = storyline;
   this.description = description;
 
-  allCards.push(this);
+  Card.allCards.push(this);
 }
-
+Card.allCards = [];
+Card.cardDeck = [];
 //++++++++++++++
 //Nightmare Card
-new cardCreate('Meet Death', 'You are strong child, but I am beyond strength ', 'all survival stats set to zero' );
+new Card(1, 'Meet Death', 'You are strong child, but I am beyond strength ', 'all survival stats set to zero' );
 
-new cardCreate('Get Robbed', '', 'Drop to 1 resources');
+new Card(2, 'Get Robbed', '', 'Drop to 1 resources');
 
-new cardCreate('find/witness bad thing' ,'', 'Drop to 1 hope');
+new Card(3, 'find/witness bad thing' ,'', 'Drop to 1 hope');
 
-new cardCreate('Lose shelter','', 'Drop to 1 protection')
+new Card(4, 'Lose shelter','', 'Drop to 1 protection')
 
-new cardCreate('Get mauled','', 'Drop to 1 hp')
+new Card(5, 'Get mauled','', 'Drop to 1 hp')
 //++++++++++++++++++
 //Net Positive Cards
-new cardCreate('Signs of Society' , 'You see signs of soceity', '+hope');
+new Card(6, 'Signs of Society' , 'You see signs of soceity', '+hope');
 
-new cardCreate('Good night Sleep', 'You slept well despite your pains', '+health')
+new Card(7, 'Good night Sleep', 'You slept well despite your pains', '+health')
 
-new cardCreate('find primitive weapon', 'find a sharp rock or club like stick', '+resource')
+new Card(8, 'find primitive weapon', 'find a sharp rock or club like stick', '+resource')
 
-new cardCreate('Found shelter during storm', 'You find a place to weather to storm', '+ protection')
+new Card(9, 'Found shelter during storm', 'You find a place to weather to storm', '+ protection')
 //++++++++++++++++++
 //Net Negative Cards
-new cardCreate('Bear.', 'The local wildlife', '--hp')
+new Card(10, 'Bear.', 'The local wildlife', '--hp')
 
-new cardCreate('Solitude', 'you miss another human being' , '--hope')
+new Card(11, 'Solitude', 'you miss another human being' , '--hope')
 
+new Card(12, 'Banditos', 'Your stash was pilfered', '--resources')
 
-new cardCreate('Banditos', 'Your stash was pilfered', '--resources')
-
-new cardCreate('Shelter destroyed', '', '--Protection')
+new Card(13, 'Shelter destroyed', '', '--Protection')
 //++++++++++++++++++++++++++++++++++
 //Mostly Negative Hybrid Value Cards
-new cardCreate('Meet merchant','He is selling apples', '-- resource + hp')
+new Card(14, 'Meet merchant','He is selling apples', '-- resource + hp')
 
-new cardCreate('Build fire', 'You prepare for the elements', '--resource +protection')
+new Card(15, 'Build fire', 'You prepare for the elements', '--resource +protection')
 
-new cardCreate('Bad Water ','--hope +hp')
+new Card(16, 'Bad Water ','--hope +hp')
 
-new cardCreate('Chased fun thing','','--protection +hope')
+new Card(17, 'Chased fun thing','','--protection +hope')
 
-new cardCreate('Find corpse' ,'Ew', '--hope +resources')
+new Card(18, 'Find corpse' ,'Ew', '--hope +resources')
 
 //++++++++++++++++++++++++++++++++++
 //Mostly Positive Hybrid Value Cards
-new cardCreate('Exhausting Hunt', 'You spend hours running down a deer', '++nutriton -hp')
+new Card(19, 'Exhausting Hunt', 'You spend hours running down a deer', '++nutriton -hp')
 
 
 //++++++++++++++++++
 //Triple Value Cards
-new cardCreate('Meet merchant', 'He is selling warm clothes', '--- resource + protection +hope')
+new Card(20, 'Meet merchant', 'He is selling warm clothes', '--- resource + protection +hope')
 
 
 //++++++++++++++++++
 //Random Chance Card
-// new cardCreate('Approach stranger', 'make friend or get robbed' , '++ socialization OR -hope -socializtion')
+// new Card('Approach stranger', 'make friend or get robbed' , '++ socialization OR -hope -socializtion')
 
 // function approachStranger(){
 //     var temp = Math.round(Math.random);
