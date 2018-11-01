af7fd8d9a9b431014e069e0a2fe5e531927e72d6
Michael <Jahnsmic001@gmail.com>
2018-10-31 17:54:04 -0700

    Fixes long standing bug of targeting text on card, also fizes subsequent bug of that fix breaking the function finder

--- app/app.js
+++ app/app.js
@@ -20,9 +20,9 @@ function drawPhase(){
   if(Card.cardDeck.length < 3){
     shuffle(Card.allCards);
   }
-  
+
   animateDraw();
-  
+
   Card.displayCard = Card.cardDeck.slice(0, 3);
   for(var i = 0; i <= 2; i++){
     var idReference = Card.cardDeck[i].id;
@@ -44,84 +44,84 @@ function turnCounter(){
 }
 //Update Game Stats
 function updateStats(){
-  document.getElementById('hopeBar').textContent = Card.gamestats["hope"];
-  document.getElementById('healthBar').textContent = Card.gamestats["health"];
-  document.getElementById('resourceBar').textContent = Card.gamestats["resource"];
-  document.getElementById('protectionBar').textContent = Card.gamestats["protection"];
+  document.getElementById('hopeBar').textContent = Card.gamestats['hope'];
+  document.getElementById('healthBar').textContent = Card.gamestats['health'];
+  document.getElementById('resourceBar').textContent = Card.gamestats['resource'];
+  document.getElementById('protectionBar').textContent = Card.gamestats['protection'];
 }
 function updateHope(operation, value){
   if(operation === 'add'){
-    Card.gamestats["hope"] += value;
+    Card.gamestats['hope'] += value;
   }
   if(operation === 'minus'){
-    Card.gamestats["hope"] -= value;
+    Card.gamestats['hope'] -= value;
   }
   if(operation === 'set'){
 
-    Card.gamestats["hope"] = value
+    Card.gamestats['hope'] = value
   }
-  if(Card.gamestats["hope"] < 0){
-    Card.gamestats["hope"] = 0;
+  if(Card.gamestats['hope'] < 0){
+    Card.gamestats['hope'] = 0;
   }
-  if(Card.gamestats["hope"] > 100){
-    Card.gamestats["hope"] = 100;
+  if(Card.gamestats['hope'] > 100){
+    Card.gamestats['hope'] = 100;
   }
   updateStats();
 }
 function updateHealth(operation, value){
   if(operation === 'add'){
-    Card.gamestats["health"] += value;
+    Card.gamestats['health'] += value;
 
   }
   if(operation === 'minus'){
-    Card.gamestats["health"] -= value;
+    Card.gamestats['health'] -= value;
   }
   if(operation === 'set'){
-    Card.gamestats["health"] = value
+    Card.gamestats['health'] = value
   }
-  if(Card.gamestats["health"] < 0){
-    Card.gamestats["health"] = 0;
+  if(Card.gamestats['health'] < 0){
+    Card.gamestats['health'] = 0;
   }
-  if(Card.gamestats["health"] > 100){
-    Card.gamestats["health"] = 100;
+  if(Card.gamestats['health'] > 100){
+    Card.gamestats['health'] = 100;
   }
   updateStats();
 }
 function updateResources(operation, value){
   if(operation === 'add'){
-    Card.gamestats["resource"] += value;
+    Card.gamestats['resource'] += value;
   }
   if(operation === 'minus'){
-    Card.gamestats["resource"] -= value;
+    Card.gamestats['resource'] -= value;
   }
   if(operation === 'set'){
-    Card.gamestats["resource"] = value
+    Card.gamestats['resource'] = value
   }
-  if(Card.gamestats["resource"] < 0){
-    Card.gamestats["resource"] = 0;
+  if(Card.gamestats['resource'] < 0){
+    Card.gamestats['resource'] = 0;
   }
-  if(Card.gamestats["resource"] > 100){
-    Card.gamestats["resource"] = 100;
+  if(Card.gamestats['resource'] > 100){
+    Card.gamestats['resource'] = 100;
   }
   updateStats();
 }
 function updateProtection(operation, value){
   if(operation === 'add'){
-    Card.gamestats["protection"] += value;
+    Card.gamestats['protection'] += value;
 
   }
   if(operation === 'minus'){
-    Card.gamestats["protection"] -= value;
+    Card.gamestats['protection'] -= value;
 
   }
   if(operation === 'set'){
-    Card.gamestats["protection"] = value
+    Card.gamestats['protection'] = value
   }
-  if(Card.gamestats["protection"] < 0){
-    Card.gamestats["protection"] = 0;
+  if(Card.gamestats['protection'] < 0){
+    Card.gamestats['protection'] = 0;
   }
-  if(Card.gamestats["protection"] > 100){
-    Card.gamestats["protection"] = 100;
+  if(Card.gamestats['protection'] > 100){
+    Card.gamestats['protection'] = 100;
   }
   updateStats();
 }
@@ -143,39 +143,44 @@ function handleStart(event){
   setTimeout(drawPhase, 6000);
 }
 function handleSelection(){
-  if(event.target.className === 'card'){
-    for(var i = 0; i < Card.allCards.length; i++){
-      if(event.target.querySelector(`h2`).innerHTML === Card.allCards[i].name)
-        var temp = Card.allCards[i].id;
-    }
-    Card.functions[temp]();
+  for(var i = 0; i < Card.allCards.length; i++){
+    if(event.target.parentNode.querySelector(`h4`).innerHTML === Card.allCards[i].name)
+      var temp = Card.allCards[i].id;
+  }
+  Card.functions[temp]();
 
     testForGameOver();
     drawPhase();
     turnCounter()
-    // saveState();
+  //   // saveState();
 
-  }
-  function testForGameOver(){
-    if(Card.gamestats["hope"] === 0 ||Card.gamestats["health"] === 0 ||Card.gamestats["resource"] === 0 || Card.gamestats["protection"] === 0){
+}
+function testForGameOver(){
+  if(Card.gamestats['hope'] === 0 ||Card.gamestats['health'] === 0 ||Card.gamestats['resource'] === 0 || Card.gamestats['protection'] === 0){
 
-      alert('You lost. You ran out of resources!')
+    alert('You lost. You ran out of resources!')
 
-      localStorage.setItem('userTurns', JSON.stringify({'clicks':Card.totalClicks}))
-      console.log(Card.totalClicks);
-      console.log(localStorage.endGameStats);
-      console.log(localStorage.submit);
-      localStorage.setItem('endGameStats', JSON.stringify({'Card.gamestats["hope"]': Card.gamestats["hope"], 'Card.gamestats["health"]':Card.gamestats["health"], 'Card.gamestats["resource"]': Card.gamestats["resource"], 'Card.gamestats["protection"]': Card.gamestats["protection"], 'clicks':Card.totalClicks}))
+    localStorage.setItem('userTurns', JSON.stringify({'clicks':Card.totalClicks}))
+    console.log(Card.totalClicks);
+    console.log(localStorage.endGameStats);
+    console.log(localStorage.submit);
+    localStorage.setItem('endGameStats', JSON.stringify({'Card.gamestats["hope"]': Card.gamestats['hope'], 'Card.gamestats["health"]':Card.gamestats['health'], 'Card.gamestats["resource"]': Card.gamestats['resource'], 'Card.gamestats["protection"]': Card.gamestats['protection'], 'clicks':Card.totalClicks}))
 
 
-      window.location.href = 'gameOver.html';
-    }
+    window.location.href = 'gameOver.html';
   }
 }
+
 //++++++++++++++
 //ExecutableCode
 
 var startElement = document.getElementById('start');
-var containerEL = document.getElementById('container');
 startElement.addEventListener('click', handleStart);
-containerEL.addEventListener('click', handleSelection);
+
+
+var cardEl0 = document.getElementById('card0')
+cardEl0.addEventListener('click', handleSelection)
+var cardEl1 = document.getElementById('card1')
+cardEl1.addEventListener('click', handleSelection)
+var cardEl2 = document.getElementById('card2')
+cardEl2.addEventListener('click', handleSelection)
